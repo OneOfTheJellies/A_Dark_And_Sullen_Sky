3b2bb2544f80a995d4cd5402c229b763bbf3bbff
One of the Jellies <kbs017260@gnspes.ca>
2024-05-27 08:18:04 -0300

    more edits - kill not working

--- Test project/Damagable.gd
+++ Test project/Damagable.gd
@@ -6,7 +6,7 @@ func _ready():
 	health = get_parent().health
 
 func getHit(damage):
-	health -= damage
+#	health -= damage
 
-	if health <= 0:
-		get_parent().die()
+#	if health <= 0:
+	get_parent().die()
