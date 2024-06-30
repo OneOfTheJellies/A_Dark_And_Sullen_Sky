5a2b244eb0c8ccf0c00f050eac5f8dce001ff192
PixiePeace <forestsspam@gmail.com>
2024-06-30 15:50:32 -0300

    Started awwdwdwing swordwww wswtwowpw stop OneOfTheJellies

---
+++ BADSCRIPT.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("item interact"):
+		translate(Vector2(0, -100))
+		rotation = 0
